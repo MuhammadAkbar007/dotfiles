a3610cea4c931fac14f735abcc25e3a0e22a07e7
MuhammadAkbar007 <fotismenos007@mail.ru>
2026-03-17 19:46:05 +0500

    bell icon changed 🔔

--- .config/waybar/toggle_nightlight.sh
+++ .config/waybar/toggle_nightlight.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# Path to the binary
+BIN="wlsunset"
+
 if [ "$1" == "status" ]; then
-    if pgrep wlsunset > /dev/null; then
-        echo '{"text": "󰖔", "class": "active", "tooltip": "Night Light: On"}'
+    if pgrep "$BIN" > /dev/null; then
+		echo '{"text": "󰖔", "class": "active", "tooltip": "Night Light: Active (Auto-transitioning)"}'
     else
-        echo '{"text": "󰖔", "class": "inactive", "tooltip": "Night Light: Off"}'
+		echo '{"text": "󰖙", "class": "inactive", "tooltip": "Night Light: Inactive"}'
     fi
     exit 0
 fi
 
 # Logic for toggling (on-click)
-if pgrep wlsunset > /dev/null; then
-    pkill wlsunset
+if pgrep "$BIN" > /dev/null; then
+    pkill "$BIN"
 else
     wlsunset -l 41.005 -L 71.794 -T 6500 -t 4000 &
 fi
