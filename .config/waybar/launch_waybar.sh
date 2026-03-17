4dca778a0eee1d8fc69be9ee20b5d3b7efe57518
MuhammadAkbar007 <fotismenos007@mail.ru>
2026-03-17 22:04:52 +0500

    moved from swaync to mako 💅

--- .config/waybar/launch_waybar.sh
+++ .config/waybar/launch_waybar.sh
@@ -3,7 +3,7 @@
 # Kill only the EXACT process named 'waybar'
 # -x ensures it doesn't kill 'launch_waybar.sh'
 pkill -x waybar
-killall -q swaync-client
+# killall -q swaync-client
 
 # Wait for it to fully shut down
 while pgrep -x waybar >/dev/null; do sleep 0.1; done
