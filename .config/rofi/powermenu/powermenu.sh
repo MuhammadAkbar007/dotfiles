d76fd89b3783d3146bc8d6b30d16bee608997b8b
akbarhbai <akbarjon.ahmadjonov.hbai@gmail.com>
2026-06-17 21:33:57 +0500

    powermenu with vim motions and wrap

--- .config/rofi/powermenu/powermenu.sh
+++ .config/rofi/powermenu/powermenu.sh
@@ -15,7 +15,6 @@ theme='style-1'
 
 # CMDs
 uptime="$(uptime -p | sed -e 's/up //g')"
-host=$(hostname)
 # Full name from the GECOS field, falling back to the login name
 fullname="$(getent passwd "$USER" | cut -d: -f5 | cut -d, -f1)"
 fullname="${fullname:-$USER}"
@@ -26,8 +25,6 @@ reboot=''
 lock=''
 suspend=''
 logout=''
-yes=''
-no=''
 
 # Rofi CMD
 rofi_cmd() {
@@ -37,41 +34,23 @@ rofi_cmd() {
         -theme ${dir}/${theme}.rasi
 }
 
-# Confirmation CMD
-confirm_cmd() {
-    rofi -dmenu \
-        -p 'Confirmation' \
-        -mesg 'Are you Sure?' \
-        -theme ${dir}/shared/confirm.rasi
-}
-
-# Ask for confirmation
-confirm_exit() {
-    echo -e "$yes\n$no" | confirm_cmd
-}
-
 # Pass variables to rofi dmenu
 run_rofi() {
     echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
-# Execute Command
+# Execute Command (no confirmation)
 run_cmd() {
-    selected="$(confirm_exit)"
-    if [[ "$selected" == "$yes" ]]; then
-        if [[ $1 == '--shutdown' ]]; then
-            systemctl poweroff
-        elif [[ $1 == '--reboot' ]]; then
-            systemctl reboot
-        elif [[ $1 == '--suspend' ]]; then
-            command -v mpc >/dev/null && mpc -q pause
-            command -v amixer >/dev/null && amixer set Master mute
-            systemctl suspend
-        elif [[ $1 == '--logout' ]]; then
-            i3-msg exit
-        fi
-    else
-        exit 0
+    if [[ $1 == '--shutdown' ]]; then
+        systemctl poweroff
+    elif [[ $1 == '--reboot' ]]; then
+        systemctl reboot
+    elif [[ $1 == '--suspend' ]]; then
+        command -v mpc >/dev/null && mpc -q pause
+        command -v amixer >/dev/null && amixer set Master mute
+        systemctl suspend
+    elif [[ $1 == '--logout' ]]; then
+        i3-msg exit
     fi
 }
 
